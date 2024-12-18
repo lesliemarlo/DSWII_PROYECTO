3a9787d44f27d9e21cd6e9ded9d0aad34e01c9d0
Zarela Eugenia Manrique Condori <zarelamanrique87@gmail.com>
2024-12-18 09:45:45 -0500

    Delete WS_REST_ms_sgb_inventario directory

--- WS_REST_ms_sgb_inventario/ms-sgb-inventario_V0.2/src/main/java/pe/edu/cibertec/dawii/ms/ecc/inventario/repository/CopiaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.cibertec.dawii.ms.ecc.inventario.repository;
-  
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pe.edu.cibertec.dawii.ms.ecc.inventario.model.Copia;
-
-public interface CopiaRepository extends JpaRepository<Copia,Long>{
- 
-}
